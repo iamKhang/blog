8fe5e0cffbd03196b37775892c71985da506f1b1
iamKhang <iamhoangkhang@icloud.com>
2024-09-14 17:14:45 +0700

    destroy backend

--- back-end/src/utils/mongoose.js
+++
@@ -1,9 +0,0 @@
-// Path: back-end/utils/mongoose.js
-module.exports = {
-    multipaleMongooseToObject: function (mongooseArrays){
-        return mongooseArrays.map(mongoose => mongoose.toObject())
-    },
-    mongooseToObject: function(mongoose){
-        return mongoose ? mongoose.toObject() : mongoose;
-    }
-}
